e6144d52121b48b33e02a9d44176fc71e777b11c
Joost Heijkoop <joostheijkoop@gmail.com>
2015-08-20 05:28:51 +0200

    Working result

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,12 @@
 package stamina.demo
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{ActorSystem, Props}
+import akka.persistence.PersistentActor
+import spray.json.lenses.JsonLenses._
+import stamina.demo.Parrot.Incrementet
+import stamina.json.SprayJsonMacros._
+import stamina.json._
+import stamina.{Persistable, Persisters, StaminaAkkaSerializer, V1, V2, V3}
 
 import scala.concurrent.duration.DurationInt
 
@@ -19,14 +25,38 @@ object Main extends App {
   }
 }
 
-class Parrot extends Actor {
+class Parrot extends PersistentActor {
   var count = 0
 
-  def receive = {
-    case message: String => println(s"$count: $message")
+  override def receiveRecover: Receive = {
+    case e @ Incrementet(_count, message) =>
+      count = _count
+      println(e)
   }
+
+  override def receiveCommand: Receive = {
+    case message: String =>
+      persist(Incrementet(count + 1, message)) { event =>
+        count = event.count
+        println(s"$count: ${event.foobar}")
+      }
+  }
+
+  override def persistenceId: String = "parrot"
 }
 
 object Parrot {
+  case class Incrementet(count: Int, foobar: String) extends Persistable
+
+  val parrotPersister = persister[Incrementet, V3]("inc", from[V1]
+    .to[V2](_.update('message ! set[String]("[empty message]")))
+    .to[V3](_.update('message ! set[String]("[empty message2]"))))
+
   val props = Props[Parrot]
+}
+
+class PricingAkkaSerializer(persisters: Persisters) extends StaminaAkkaSerializer(persisters) {
+  def this() {
+    this(Persisters(List(Parrot.parrotPersister)))
+  }
 }
