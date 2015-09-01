d75fb30edee6163be7daa4ae9fe72331a9fa46ec
Joost Heijkoop <joostheijkoop@gmail.com>
2015-09-02 00:28:58 +0200

    Add changes that resulted in the end result at meetup

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,7 +6,7 @@ import spray.json.lenses.JsonLenses._
 import stamina.demo.Parrot.Incremented
 import stamina.json.SprayJsonMacros._
 import stamina.json._
-import stamina.{Persistable, Persisters, StaminaAkkaSerializer, V1, V2}
+import stamina.{V2, Persistable, Persisters, StaminaAkkaSerializer, V1, V3}
 
 import scala.concurrent.duration.DurationInt
 
@@ -29,7 +29,9 @@ class Parrot extends PersistentActor {
   var count = 0
 
   override def receiveRecover: Receive = {
-    case event: Incremented => update(event)
+    case event: Incremented =>
+      update(event)
+      print(event)
   }
 
   override def receiveCommand: Receive = {
@@ -49,10 +51,11 @@ object Parrot {
   case class Incremented(count: Int, message: String) extends Persistable
   val props = Props[Parrot]
 
-  val parrotPersister = persister[Incremented, V2](
+  val parrotPersister = persister[Incremented, V3](
     "increment",
     from[V1]
-      .to[V2](_.update('message ! set[String]("[empty message]")))
+      .to[V2](_.update('message ! set[String]("[empty message]"))
+      .to[V3](_.update('message ! set[String]("[empty message]")))
   )
 }
 
